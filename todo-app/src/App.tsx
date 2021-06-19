e35e7113ff8a074314189b17e299af1294d7f906
akshay gajjar <akshaygajjar101@gmail.com>
2021-06-19 12:40:18 +0530

    Getting User Input with refs

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FunctionComponent = () => {
-  return <div className='App'>HELLO TYPESCRIPT</div>;
+  const todos = [{ id: 'T1', text: 'finish the task' }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
+  return (
+    <div className='App'>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
