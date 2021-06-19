cfaacb57c3403ed64475134bdf738442850a61e6
akshay gajjar <akshaygajjar101@gmail.com>
2021-06-19 13:03:40 +0530

    Adding Todos with State & Types

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FunctionComponent = () => {
-  const todos = [{ id: 'T1', text: 'finish the task' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
